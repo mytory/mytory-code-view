264a142b33d5402aa1dfe026b1b9bc5d9a1d7e1c
mytory <mytory@gmail.com>
2013-09-18 20:15:24 +0900

    window resize에 대응

--- ko-KR.js
+++ ko-KR.js
@@ -1,13 +1,26 @@
 jQuery(document).ready(function(){
+
+    var add_open_new_window_for_code = function(){
+        $('.content pre').each(function(){
+            if(this.scrollWidth > this.clientWidth){
+                $(this).before($('<a/>',{
+                    text: '▷코드 새 창에서 보기',
+                    'class': 'js-open-mcv-code-view',
+                    'href': '#'
+                }));
+            }
+        });
+        $('.js-open-mcv-code-view').click(function(e){
+            e.preventDefault();
+            $('.mcv-code-view-form__input').val($(this).next().html());
+            $('.mcv-code-view-form').submit();
+        });
+    };
+
     var mb = $('.content pre').css('margin-bottom');
-    $('.content pre').before($('<a/>',{
-        text: '▷코드 새 창에서 보기',
-        'class': 'js-open-mcv-code-view',
-        'href': '#'
-    }));
-    $('.js-open-mcv-code-view').click(function(e){
-        e.preventDefault();
-        $('.mcv-code-view-form__input').val($(this).next().html());
-        $('.mcv-code-view-form').submit();
+    add_open_new_window_for_code();
+    $(window).resize(function(){
+        $('.js-open-mcv-code-view').remove();
+        add_open_new_window_for_code();
     });
 });
