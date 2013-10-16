2f88131ed9787085d2b669e0398f69496ac2d4ee
mytory <mytory@gmail.com>
2013-10-16 23:46:42 +0900

    code 태그를 좀더 잘 넣도록 함.

--- code-view.php
+++ code-view.php
@@ -3,6 +3,14 @@ if( ! isset($_POST['pre']) OR empty($_POST['pre'])){
     echo "I need codes.";
     exit;
 }
+
+function cb_print_code($code){
+    if(strstr($code, '<code')){
+        echo $code;
+    }else{
+        echo "<code>{$code}</code>";
+    }
+}
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
@@ -14,6 +22,6 @@ if( ! isset($_POST['pre']) OR empty($_POST['pre'])){
     <script type="text/javascript">hljs.initHighlightingOnLoad();</script>
 </head>
 <body style="margin:0;background:#272822">
-    <pre style="margin:0"><code><? echo $_POST['pre']?></code></pre>
+    <pre style="margin:0"><?php cb_print_code($_POST['pre'])?></pre>
 </body>
 </html>
